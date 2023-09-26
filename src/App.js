35d9fc34bc9a52770798b9abb3aaf84d5007b265
Majid Marzbani <majjidmarzbani9@gmail.com>
2023-09-26 18:51:22 +0330

    in this commit i changed the routing and used react-router-dom  so the page does not refresh when clicking on differrent links

--- src/App.js
+++ src/App.js
@@ -3,30 +3,20 @@ import Pricing from "./pages/Pricing";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-
-
-  let component;
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home />;
-      break;
-    case "/pricing":
-      component = <Pricing />;
-      break;
-    case "/about":
-      component = <About />;
-      break;
-    default:
-      component = <Home />;
-      break;
-  }
+  
   return (
     <>
       <Navbar />
-      <div className="container">{component}</div>
+      <div className="container">
+        <Routes>
+          <Route path="/" element = {<Home />} />
+          <Route path="/pricing" element = {<Pricing />} />
+          <Route path="/about" element = {<About />} />
+        </Routes>
+      </div>
     </>
   );
 }
